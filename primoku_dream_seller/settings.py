1b0411ae311008c7c8b347cddd5a6701ea9f2772
PrimoKu <primoku.dev@gmail.com>
2022-05-01 21:35:06 +0800

    2022-05-01 templates dir path

--- primoku_dream_seller/settings.py
+++ primoku_dream_seller/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'primoku_dream_seller.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ 'templates' ],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
